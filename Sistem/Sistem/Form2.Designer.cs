a981a228b98a613ce8a7323336cad00c1ac8a456
aa12v <133629221+aa12v@users.noreply.github.com>
2023-09-28 15:46:26 +0300

    ax

---
+++ Sistem/Sistem/Form2.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Sistem
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label_saatDakikaSaniye = new System.Windows.Forms.Label();
+            this.label_günAyYil = new System.Windows.Forms.Label();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.pROFİLAYARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pROFİLDÜZENLEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.şİFREDEĞİŞTİRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dURUMBİLGİToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELBİLGİToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELPROFİLİNCELEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELPROFİLDÜZENLEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELPROFİLSORGULAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELEKLEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELSİLToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELİZİNDURUMToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTBİLGİToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTPROFİLİNCELEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTGÜNCELLEToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTBARKODSORGULAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTEKLEToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTSİLToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.kARTTESLİMETToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.gÜVENLİÇIKIŞToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(364, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(423, 102);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 65;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label_saatDakikaSaniye
+            // 
+            this.label_saatDakikaSaniye.AutoSize = true;
+            this.label_saatDakikaSaniye.Font = new System.Drawing.Font("Arial Black", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_saatDakikaSaniye.Location = new System.Drawing.Point(892, 148);
+            this.label_saatDakikaSaniye.Name = "label_saatDakikaSaniye";
+            this.label_saatDakikaSaniye.Size = new System.Drawing.Size(25, 27);
+            this.label_saatDakikaSaniye.TabIndex = 67;
+            this.label_saatDakikaSaniye.Text = "0";
+            // 
+            // label_günAyYil
+            // 
+            this.label_günAyYil.AutoSize = true;
+            this.label_günAyYil.Font = new System.Drawing.Font("Arial Black", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label_günAyYil.Location = new System.Drawing.Point(993, 148);
+            this.label_günAyYil.Name = "label_günAyYil";
+            this.label_günAyYil.Size = new System.Drawing.Size(25, 27);
+            this.label_günAyYil.TabIndex = 68;
+            this.label_günAyYil.Text = "0";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.menuStrip2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pROFİLAYARToolStripMenuItem,
+            this.dURUMBİLGİToolStripMenuItem,
+            this.gÜVENLİÇIKIŞToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(1420, 24);
+            this.menuStrip2.TabIndex = 69;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // pROFİLAYARToolStripMenuItem
+            // 
+            this.pROFİLAYARToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pROFİLDÜZENLEToolStripMenuItem,
+            this.şİFREDEĞİŞTİRToolStripMenuItem});
+            this.pROFİLAYARToolStripMenuItem.Name = "pROFİLAYARToolStripMenuItem";
+            this.pROFİLAYARToolStripMenuItem.Size = new System.Drawing.Size(92, 20);
+            this.pROFİLAYARToolStripMenuItem.Text = "PROFİL AYAR";
+            // 
+            // pROFİLDÜZENLEToolStripMenuItem
+            // 
+            this.pROFİLDÜZENLEToolStripMenuItem.Name = "pROFİLDÜZENLEToolStripMenuItem";
+            this.pROFİLDÜZENLEToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.pROFİLDÜZENLEToolStripMenuItem.Text = "PROFİL DÜZENLE";
+            // 
+            // şİFREDEĞİŞTİRToolStripMenuItem
+            // 
+            this.şİFREDEĞİŞTİRToolStripMenuItem.Name = "şİFREDEĞİŞTİRToolStripMenuItem";
+            this.şİFREDEĞİŞTİRToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+            this.şİFREDEĞİŞTİRToolStripMenuItem.Text = "ŞİFRE DEĞİŞTİR";
+            // 
+            // dURUMBİLGİToolStripMenuItem
+            // 
+            this.dURUMBİLGİToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pERSONELBİLGİToolStripMenuItem,
+            this.kARTBİLGİToolStripMenuItem});
+            this.dURUMBİLGİToolStripMenuItem.Name = "dURUMBİLGİToolStripMenuItem";
+            this.dURUMBİLGİToolStripMenuItem.Size = new System.Drawing.Size(93, 20);
+            this.dURUMBİLGİToolStripMenuItem.Text = "DURUM BİLGİ";
+            // 
+            // pERSONELBİLGİToolStripMenuItem
+            // 
+            this.pERSONELBİLGİToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pERSONELPROFİLİNCELEToolStripMenuItem,
+            this.pERSONELPROFİLDÜZENLEToolStripMenuItem,
+            this.pERSONELPROFİLSORGULAToolStripMenuItem,
+            this.pERSONELEKLEToolStripMenuItem,
+            this.pERSONELSİLToolStripMenuItem1,
+            this.pERSONELİZİNDURUMToolStripMenuItem,
+            this.pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem});
+            this.pERSONELBİLGİToolStripMenuItem.Name = "pERSONELBİLGİToolStripMenuItem";
+            this.pERSONELBİLGİToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.pERSONELBİLGİToolStripMenuItem.Text = "PERSONEL DURUM";
+            // 
+            // pERSONELPROFİLİNCELEToolStripMenuItem
+            // 
+            this.pERSONELPROFİLİNCELEToolStripMenuItem.Name = "pERSONELPROFİLİNCELEToolStripMenuItem";
+            this.pERSONELPROFİLİNCELEToolStripMenuItem.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELPROFİLİNCELEToolStripMenuItem.Text = "PERSONEL PROFİL İNCELE";
+            // 
+            // pERSONELPROFİLDÜZENLEToolStripMenuItem
+            // 
+            this.pERSONELPROFİLDÜZENLEToolStripMenuItem.Name = "pERSONELPROFİLDÜZENLEToolStripMenuItem";
+            this.pERSONELPROFİLDÜZENLEToolStripMenuItem.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELPROFİLDÜZENLEToolStripMenuItem.Text = "PERSONEL PROFİL DÜZENLE";
+            // 
+            // pERSONELPROFİLSORGULAToolStripMenuItem
+            // 
+            this.pERSONELPROFİLSORGULAToolStripMenuItem.Name = "pERSONELPROFİLSORGULAToolStripMenuItem";
+            this.pERSONELPROFİLSORGULAToolStripMenuItem.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELPROFİLSORGULAToolStripMenuItem.Text = "PERSONEL BARKOD SORGULA";
+            // 
+            // pERSONELEKLEToolStripMenuItem
+            // 
+            this.pERSONELEKLEToolStripMenuItem.Name = "pERSONELEKLEToolStripMenuItem";
+            this.pERSONELEKLEToolStripMenuItem.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELEKLEToolStripMenuItem.Text = "PERSONEL EKLE";
+            // 
+            // pERSONELSİLToolStripMenuItem1
+            // 
+            this.pERSONELSİLToolStripMenuItem1.Name = "pERSONELSİLToolStripMenuItem1";
+            this.pERSONELSİLToolStripMenuItem1.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELSİLToolStripMenuItem1.Text = "PERSONEL SİL";
+            // 
+            // pERSONELİZİNDURUMToolStripMenuItem
+            // 
+            this.pERSONELİZİNDURUMToolStripMenuItem.Name = "pERSONELİZİNDURUMToolStripMenuItem";
+            this.pERSONELİZİNDURUMToolStripMenuItem.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELİZİNDURUMToolStripMenuItem.Text = "PERSONEL İZİN DURUM";
+            // 
+            // pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem
+            // 
+            this.pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem.Name = "pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem";
+            this.pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem.Size = new System.Drawing.Size(274, 22);
+            this.pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem.Text = "PERSONEL İŞE GİRİŞ / ÇIKIŞ DURUM";
+            // 
+            // kARTBİLGİToolStripMenuItem
+            // 
+            this.kARTBİLGİToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kARTPROFİLİNCELEToolStripMenuItem,
+            this.kARTGÜNCELLEToolStripMenuItem1,
+            this.kARTBARKODSORGULAToolStripMenuItem,
+            this.kARTEKLEToolStripMenuItem1,
+            this.kARTSİLToolStripMenuItem1,
+            this.kARTTESLİMETToolStripMenuItem1});
+            this.kARTBİLGİToolStripMenuItem.Name = "kARTBİLGİToolStripMenuItem";
+            this.kARTBİLGİToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.kARTBİLGİToolStripMenuItem.Text = "KART DURUM";
+            // 
+            // kARTPROFİLİNCELEToolStripMenuItem
+            // 
+            this.kARTPROFİLİNCELEToolStripMenuItem.Name = "kARTPROFİLİNCELEToolStripMenuItem";
+            this.kARTPROFİLİNCELEToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.kARTPROFİLİNCELEToolStripMenuItem.Text = "KART PROFİL İNCELE";
+            // 
+            // kARTGÜNCELLEToolStripMenuItem1
+            // 
+            this.kARTGÜNCELLEToolStripMenuItem1.Name = "kARTGÜNCELLEToolStripMenuItem1";
+            this.kARTGÜNCELLEToolStripMenuItem1.Size = new System.Drawing.Size(216, 22);
+            this.kARTGÜNCELLEToolStripMenuItem1.Text = "KART PROFİL GÜNCELLE";
+            // 
+            // kARTBARKODSORGULAToolStripMenuItem
+            // 
+            this.kARTBARKODSORGULAToolStripMenuItem.Name = "kARTBARKODSORGULAToolStripMenuItem";
+            this.kARTBARKODSORGULAToolStripMenuItem.Size = new System.Drawing.Size(216, 22);
+            this.kARTBARKODSORGULAToolStripMenuItem.Text = "KART BARKOD SORGULA";
+            // 
+            // kARTEKLEToolStripMenuItem1
+            // 
+            this.kARTEKLEToolStripMenuItem1.Name = "kARTEKLEToolStripMenuItem1";
+            this.kARTEKLEToolStripMenuItem1.Size = new System.Drawing.Size(216, 22);
+            this.kARTEKLEToolStripMenuItem1.Text = "KART EKLE";
+            // 
+            // kARTSİLToolStripMenuItem1
+            // 
+            this.kARTSİLToolStripMenuItem1.Name = "kARTSİLToolStripMenuItem1";
+            this.kARTSİLToolStripMenuItem1.Size = new System.Drawing.Size(216, 22);
+            this.kARTSİLToolStripMenuItem1.Text = "KART SİL";
+            // 
+            // kARTTESLİMETToolStripMenuItem1
+            // 
+            this.kARTTESLİMETToolStripMenuItem1.Name = "kARTTESLİMETToolStripMenuItem1";
+            this.kARTTESLİMETToolStripMenuItem1.Size = new System.Drawing.Size(216, 22);
+            this.kARTTESLİMETToolStripMenuItem1.Text = "KART TESLİM ET";
+            // 
+            // gÜVENLİÇIKIŞToolStripMenuItem
+            // 
+            this.gÜVENLİÇIKIŞToolStripMenuItem.Name = "gÜVENLİÇIKIŞToolStripMenuItem";
+            this.gÜVENLİÇIKIŞToolStripMenuItem.Size = new System.Drawing.Size(101, 20);
+            this.gÜVENLİÇIKIŞToolStripMenuItem.Text = "GÜVENLİ ÇIKIŞ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1420, 763);
+            this.Controls.Add(this.menuStrip2);
+            this.Controls.Add(this.label_günAyYil);
+            this.Controls.Add(this.label_saatDakikaSaniye);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PROVENT";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label_saatDakikaSaniye;
+        private System.Windows.Forms.Label label_günAyYil;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem pROFİLAYARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pROFİLDÜZENLEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem şİFREDEĞİŞTİRToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dURUMBİLGİToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELBİLGİToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELPROFİLİNCELEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELPROFİLDÜZENLEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELPROFİLSORGULAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELEKLEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELSİLToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELİZİNDURUMToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pERSONELİŞEGİRİŞÇIKIŞToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kARTBİLGİToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kARTPROFİLİNCELEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kARTGÜNCELLEToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kARTBARKODSORGULAToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kARTEKLEToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kARTSİLToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem kARTTESLİMETToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem gÜVENLİÇIKIŞToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+    }
+}

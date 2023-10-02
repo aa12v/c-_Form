83f7b59a1e11a81847aba39e0103e330bb5f0364
aa12v <133629221+aa12v@users.noreply.github.com>
2023-10-02 12:16:36 +0300

    a2

---
+++ Sistem2/Sistem/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Sql;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace Sistem
+{
+    public partial class Form_KullaniciGiris : Form
+    {
+        public Form_KullaniciGiris()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-6GFGG91\\SQLEXPRESS;Initial Catalog=kontrol;Integrated Security=True");
+
+        private void Form_KullaniciGiris_Load(object sender, EventArgs e)
+        {
+
+            pictureBox_gizliAcik.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox_gizliAcik.Image = Image.FromFile("C:\\Users\\provent_yazilim\\Desktop\\ProjeResimler\\kapali.png");
+
+            textBox_kullaniciParola.UseSystemPasswordChar = true;
+
+        }
+
+
+        private void button_GirisYap_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("SELECT * FROM kullanici WHERE kullaniciAd = @kullaniciAd AND parola = @parola", baglanti);
+            komut.Parameters.AddWithValue("@kullaniciAd", textBox_kullaniciAd.Text);
+            komut.Parameters.AddWithValue("@parola", textBox_kullaniciParola.Text);
+
+            SqlDataReader dr = komut.ExecuteReader();
+
+            try
+            {
+                if (dr.Read())
+                {
+                    string kullaniciRol = dr["rol"].ToString().Trim();
+
+                    switch (kullaniciRol)
+                    {
+                        case "form2":
+                            Form11 form2 = new Form11();
+                            form2.Show();
+                            this.Hide();
+                            break;
+                        case "form3":
+                            Form3 form3 = new Form3();
+                            form3.Show();
+                            this.Hide();
+                            break;
+                        case "form4":
+                            Form4 form4 = new Form4();
+                            form4.Show();
+                            this.Hide();
+                            break;
+                        case "form5":
+                            Form5 form5 = new Form5();
+                            form5.Show();
+                            this.Hide();
+                            break;
+                        case "form6":
+                            Form6 form6 = new Form6();
+                            form6.Show();
+                            this.Hide();
+                            break;
+                        case "form7":
+                            Form7 form7 = new Form7();
+                            form7.Show();
+                            this.Hide();
+                            break;
+                        case "form8":
+                            Form8 form8 = new Form8();
+                            form8.Show();
+                            this.Hide();
+                            break;
+                        case "form9":
+                            Form9 form9 = new Form9();
+                            form9.Show();
+                            this.Hide();
+                            break;
+                        default:
+                            MessageBox.Show("Geçersiz kullanıcı rolü.");
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Kullanıcı adı veya parola yanlış.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                baglanti.Close();
+            }
+        }
+
+        int  gizliAcik = 0;
+
+        private void pictureBox_gizliAcik_Click(object sender, EventArgs e)
+        {
+            if (gizliAcik == 0)
+            {
+                pictureBox_gizliAcik.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox_gizliAcik.Image = Image.FromFile("C:\\Users\\provent_yazilim\\Desktop\\ProjeResimler\\kapali.png");
+                textBox_kullaniciParola.UseSystemPasswordChar = true;
+                gizliAcik = 1;
+
+            }
+            else if (gizliAcik == 1)
+            {
+                pictureBox_gizliAcik.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBox_gizliAcik.Image = Image.FromFile("C:\\Users\\provent_yazilim\\Desktop\\ProjeResimler\\acik.png");
+                textBox_kullaniciParola.UseSystemPasswordChar = false;
+                gizliAcik = 0;
+
+
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label_günAyYil.Text = ((DateTime.Now.Day) + "." + (DateTime.Now.Month) + "." + (DateTime.Now.Year)).ToString();
+            label_saatDakikaSaniye.Text = ((DateTime.Now.Hour) + "." + (DateTime.Now.Minute) + "." + (DateTime.Now.Second)).ToString();
+        }
+    }
+}
---
+++ Sistem2/Sistem/Form11.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.Aztec;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace Sistem
+{
+    public partial class Form11 : Form
+    {
+        public Form11()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        static string conString = "Data Source=DESKTOP-6GFGG91\\SQLEXPRESS;Initial Catalog=kontrol;Integrated Security=True";
+        SqlConnection connect = new SqlConnection(conString);
+
+        PrintDialog PRD = new PrintDialog();
+
+        private void Form11_Load(object sender, EventArgs e)
+        {
+            label_barcodeCode.Visible = false;
+            textBox_barcode.Visible = false;
+
+            DoldurComboBox();
+
+            comboBox_kimdenAldiIceri_kimeVerdiDisari.Items.Add("Öğe 1");
+            comboBox_kimdenAldiIceri_kimeVerdiDisari.Items.Add("Öğe 2");
+            comboBox_kimdenAldiIceri_kimeVerdiDisari.Items.Add("Öğe 3");
+
+            // Başlangıçta seçili öğeyi belirtmek için aşağıdaki kodu kullanın
+            comboBox_kimdenAldiIceri_kimeVerdiDisari.SelectedIndex = 0;
+
+
+
+        }
+
+
+
+
+
+
+        public void KayitlariGetir()
+        {
+            // DataGridView'i doldurmak için SQL sorgusu
+            string getirSorgusu = "SELECT idKart, eylemKart, kartKodu, kartAdet, kartDurumAciklama, barcodeKart, elektronikPersonel, disPersonel FROM kart";
+
+            SqlCommand komut = new SqlCommand(getirSorgusu, connect);
+            SqlDataAdapter adapter = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+
+            connect.Open();
+            adapter.Fill(dt);
+            connect.Close();
+
+            dataGridView1.DataSource = dt;
+        }
+
+
+
+
+
+
+
+        public void veriSil(int idd)
+        {
+            string sil = "DELETE FROM kart WHERE idKart = @idd";
+
+            SqlCommand komut = new SqlCommand(sil, connect);
+            connect.Open();
+            komut.Parameters.AddWithValue("@idd", idd);
+            komut.ExecuteNonQuery();
+            connect.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label_günAyYil.Text = DateTime.Now.ToString("dd.MM.yyyy");
+            label_saatDakikaSaniye.Text = DateTime.Now.ToString("HH.mm.ss");
+        }
+
+        private void button_kayitlariListele_Click(object sender, EventArgs e)
+        {
+            KayitlariGetir();
+        }
+
+        private void button_personelAra_Click(object sender, EventArgs e)
+        {
+            string ara = "SELECT * FROM kart k INNER JOIN kullanici u ON k.kullaniciId = u.kullaniciId WHERE k.eylemKart LIKE '%' + @eylemKart + '%' OR k.barcodeKart = @barcodeKart OR u.isim LIKE '%' + @isim + '%' OR u.soyisim LIKE '%' + @soyisim + '%'";
+            SqlCommand komut = new SqlCommand(ara, connect);
+
+            komut.Parameters.AddWithValue("@eylemKart", textBox_araKart.Text);
+            komut.Parameters.AddWithValue("@barcodeKart", textBox_araKart.Text);
+            komut.Parameters.AddWithValue("@isim", textBox_araKart.Text);
+            komut.Parameters.AddWithValue("@soyisim", textBox_araKart.Text);
+
+            SqlDataAdapter ad = new SqlDataAdapter(komut);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+            connect.Close();
+        }
+
+        private void DoldurComboBox()
+        {
+
+            comboBox_kimAldi_kimVerdi.Items.Clear();
+            // Veritabanı bağlantısını açın
+            connect.Open();
+
+            // Kullanıcı adı ve soyadı bilgilerini içeren SQL sorgusu
+            string sorgu = "SELECT CONCAT(isim, ' ', soyisim) AS TamAd FROM kullanici";
+
+            SqlCommand komut = new SqlCommand(sorgu, connect);
+            SqlDataReader okuyucu = komut.ExecuteReader();
+
+            while (okuyucu.Read())
+            {
+                // ComboBox'a kullanıcı adı ve soyadını ekleyin
+                comboBox_kimAldi_kimVerdi.Items.Add(okuyucu["TamAd"].ToString());
+            }
+
+            // Veritabanı bağlantısını kapatın
+            connect.Close();
+        }
+
+        private void button_kaydiBarkodaDonustur_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connect.Open();
+
+                string yil = DateTime.Now.Year.ToString();
+                string ay = DateTime.Now.Month.ToString("00");
+                string gun = DateTime.Now.Day.ToString("00");
+                string saat = DateTime.Now.Hour.ToString("00");
+                string dakika = DateTime.Now.Minute.ToString("00");
+                string tarihKod = gun + ay;
+
+                textBox_barcode.Text = "*" + "(XX)XXXXXXX" + tarihKod + "(" + ")" + "*";
+
+                string secilenKullanici = comboBox_kimAldi_kimVerdi.SelectedItem.ToString();
+                string secilenEylem = comboBox_kartEylem.SelectedItem.ToString();
+                string secilenKimdenAldiIceri_kimeVerdiDisari = comboBox_kimdenAldiIceri_kimeVerdiDisari.SelectedItem.ToString();
+
+                string[] adSoyad = secilenKullanici.Split(' ');
+                string ad = adSoyad[0];
+                string soyad = adSoyad[1];
+
+                string kayit = "INSERT INTO kart (eylemKart, kartKodu, kartAdet, kartDurumAciklama, barcodeKart, elektronikPersonel, disPersonel) VALUES (@eylemKart, @kartKodu, @kartAdet, @kartDurumAciklama, @barcodeKart, @elektronikPersonel, @disPersonel)";
+                SqlCommand komut = new SqlCommand(kayit, connect);
+
+                komut.Parameters.AddWithValue("@eylemKart", secilenEylem);
+                komut.Parameters.AddWithValue("@kartKodu", comboBox_alinanKartKodu.Text);
+                komut.Parameters.AddWithValue("@kartAdet", textBox_kacAdet.Text);
+                komut.Parameters.AddWithValue("@kartDurumAciklama", textBox_kartDurum.Text);
+                komut.Parameters.AddWithValue("@barcodeKart", textBox_barcode.Text);
+                komut.Parameters.AddWithValue("@elektronikPersonel", secilenKullanici);
+                komut.Parameters.AddWithValue("@disPersonel", secilenKimdenAldiIceri_kimeVerdiDisari);
+
+                komut.ExecuteNonQuery();
+
+                MessageBox.Show("Kayıt Ekleme Başarılı");
+
+                label_barcodeCode.Visible = true;
+                textBox_barcode.Visible = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            KayitlariGetir();
+        }
+
+
+
+        private void button_kaydiSil_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow drow in dataGridView1.SelectedRows)
+            {
+                int idd = Convert.ToInt32(drow.Cells[0].Value);
+                veriSil(idd);
+                KayitlariGetir();
+            }
+        }
+
+        int i = 0;
+
+        private void button_kaydiGuncelle_Click(object sender, EventArgs e)
+        {
+            connect.Open();
+
+            string kayitGuncelle = "UPDATE kart SET eylemKart = @eylemKart, kartKodu = @kartKodu, kartAdet = @kartAdet, kartDurumAciklama = @kartDurumAciklama WHERE idKart = @idd";
+            SqlCommand komut = new SqlCommand(kayitGuncelle, connect);
+
+            komut.Parameters.AddWithValue("@eylemKart", comboBox_kartEylem.Text);
+            komut.Parameters.AddWithValue("@kartKodu", comboBox_alinanKartKodu.Text);
+            komut.Parameters.AddWithValue("@kartAdet", textBox_kacAdet.Text);
+            komut.Parameters.AddWithValue("@kartDurumAciklama", textBox_kartDurum.Text);
+
+            komut.Parameters.AddWithValue("@idd", dataGridView1.Rows[i].Cells[0].Value);
+
+            komut.ExecuteNonQuery();
+            MessageBox.Show("Kayıtlar Başarıyla Güncellendi!");
+            connect.Close();
+            KayitlariGetir();
+        }
+
+
+        private void dataGridView1_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            i = e.RowIndex;
+
+            comboBox_kartEylem.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
+            comboBox_alinanKartKodu.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
+            textBox_kacAdet.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+            textBox_kartDurum.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
+        }
+
+        private void button_BarcodeBastir_Click(object sender, EventArgs e)
+        {
+            PrintDocument kagit = new PrintDocument();
+            DialogResult yazdirmaIslemi;
+            yazdirmaIslemi = PRD.ShowDialog();
+            kagit.PrintPage += Kagit_PrintPage;
+
+            if (yazdirmaIslemi == DialogResult.OK)
+            {
+                kagit.Print();
+            }
+        }
+
+        private void Kagit_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            string neYazilacak = textBox_olusturulacakBarcode.Text;
+            Font yaziTipi = new Font("IDAHC39M Code 39 Barcode", 11);
+            SolidBrush kalem = new SolidBrush(Color.Black);
+            e.Graphics.DrawString(neYazilacak, yaziTipi, kalem, 43, 60);
+        }
+
+        private void comboBox_kartEylem_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            string secilenEylem = comboBox_kartEylem.SelectedItem.ToString();
+
+            if (secilenEylem == "Teslim Alındı")
+            {
+                label1.Text = "Teslim Alan Personel :";
+                label_aa.Text = "Teslim Veren Personel :";
+            }
+            else if (secilenEylem == "Teslim Verildi")
+            {
+                label1.Text = "Teslim Veren Personel :";
+                label_aa.Text = "Teslim Alan Personel :";
+            }
+        }
+    }
+}
